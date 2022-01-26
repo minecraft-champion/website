2020aeb71199944f3f2bb7a21ca34383fb557c4e
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-01-26 16:22:38 +0100

    feat 404 page

---
+++ elements/404.php
@@ -0,0 +1,10 @@
+<div class="404">
+<div class="presentation l-part l-no-marge background-color2">
+    <h2 class="title title-secondary">
+    Oh no! Bad url!
+    </h2>
+    <p class="subtitle">
+    Go back in a <a href="/" class="link">safe place</a>
+    </p>
+</div>
+</div>
