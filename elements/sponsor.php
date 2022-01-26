9f88af2e242702c68718ac66e606eb3cbb4a0f1c
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-01-26 16:22:25 +0100

    feat sponsor and tournament

---
+++ elements/sponsor.php
@@ -0,0 +1,99 @@
+<div class="sponsor">
+<div class="presentation l-part l-no-marge background-color2">
+    <h2 class="title-secondary title">
+        Help us!
+    </h2>
+    <p class="subtitle">
+        You love this project?
+    </p>
+    <p class="description center">
+        Do you want to help us to create the tournament? <a href="/discord" class="link">Join</a> us! <br>
+        Do you want to help us but you don't have skills? Do a <a href="https://patreon.com/" class="link link-extern" target="_blank">donation</a>!
+    </p>
+</div>
+<div class="">
+    <div class="sponsors l-part">
+        <h2 class="title title-secondary">
+            Our sponsors help us
+        </h2>
+        <p class="subtitle">
+            To grew up and improve the quality of our work
+        </p>
+        <div class="l-flex l-list2">
+            <div class="l-flex-item l-flex2-item">
+                <img class="image logo part-logo" src="https://via.placeholder.com/256x256" alt="Other Part">
+                <div class="information">
+                    <h3 class="title title-first_box">
+                        Other Part
+                    </h3>
+                    <p class="description l-flex-content l-flex2-content">
+                        Another part
+                    </p>
+                </div>
+            </div>
+            <div class="l-flex-item l-flex2-item">
+                <img class="image logo part-logo" src="https://via.placeholder.com/256x256" alt="CMFr">
+                <div class="information">
+                    <h3 class="title title-first_box">
+                        CMFr
+                    </h3>
+                    <p class="description l-flex-content l-flex2-content">
+                        <a href="https://discord.gg/cmfr" class="link link-extern" target="_blank">CMFr</a> is the french community on Discord! <br>
+                        They have more than 7000 members!
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="l-flex l-list3 l-separator">
+            <div class="l-flex-item l-flex3-item">
+                <img class="image logo part-logo" src="https://via.placeholder.com/196x196" alt="Other Part">
+                <div class="information">
+                    <h3 class="title title-first_box">
+                        Other Part
+                    </h3>
+                    <p class="description l-flex-content l-flex3-content center">
+                        They help us in the communication!
+                    </p>
+                </div>
+            </div>
+            <div class="l-flex-item l-flex3-item">
+                <img class="image logo part-logo" src="https://via.placeholder.com/196x196" alt="CMFr">
+                <div class="information">
+                    <h3 class="title title-first_box">
+                        Other Part
+                    </h3>
+                    <p class="description l-flex-content l-flex3-content center">
+                        They help us in the build
+                    </p>
+                </div>
+            </div>
+            <div class="l-flex-item l-flex3-item">
+                <img class="image logo part-logo" src="https://via.placeholder.com/196x196" alt="CMFr">
+                <div class="information">
+                    <h3 class="title title-first_box">
+                        Other Part
+                    </h3>
+                    <p class="description l-flex-content l-flex3-content center">
+                        They help us in the development
+                    </p>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="become-sponsor l-part background-color2">
+    <h2 class="title title-secondary">
+        Do you want to help us?
+    </h2>
+    <p class="subtitle">
+        Become a sponsor!
+    </p>
+    <p class="description center">
+        To become a sponsor, you just need to send us a dm on <a href="/discord" class="link">Discord</a> or an <a class="link mail-link" href="mailto:anhgelus.morhtuuzh@gmx.com">E-Mail</a>.
+    </p>
+    <p class="description center">
+        Don't forget to indicate why do you want to help us and how do you can help us!
+    </p>
+    <a href="/discord" class="btn-primary btn link">Discord</a>
+    <a href="mailto:anhgelus.morhtuuzh@gmx.com" class="btn-primary btn link">E-Mail</a>
+</div>
+</div>
---
+++ elements/tournament.php
@@ -0,0 +1,163 @@
+<div class="tournament">
+<div class="presentation l-part l-no-marge background-color2">
+    <h2 class="title title-secondary">
+        Tournaments information!
+    </h2>
+    <p class="subtitle">
+        Next match
+    </p>
+    <div class="matches">
+        <div class="match l-flex l-list3">
+            <div class="team1 l-flex-item l-flex3-item">
+                <img class="team-logo logo image" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                <a class="title title-first_box team-name link" href="/wiki/TeamName">
+                    TeamName
+                </a>
+            </div>
+            <div class="team-vs l-flex-item l-flex3-item center-vert">
+                <a class="vs center l-flex-content link" href="/tournament/match?id=ID_DU_MATCH">
+                    VS
+                </a>
+                <p class="vs-date center l-flex-content">
+                    11.10 am - Tuesday 02/18/22
+                </p>
+            </div>
+            <div class="team2 l-flex-item l-flex3-item">
+                <img class="team-logo logo image" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                <a class="title title-first_box team-name link" href="/wiki/TeamName">
+                    TeamName
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="is-little-screen" style="display: none">
+        <a class="btn btn-primary center link" href="/tournament/all-matches">See all matches</a>
+    </div>
+    <h3 class="title title-secondary l-little-separator not-important">
+        Every matches
+    </h3>
+    <div class="matches list not-important">
+        <ul>
+            <li class="match l-flex l-list3">
+                <div class="team1 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+                <div class="team-vs l-flex-item l-flex3-item center-vert">
+                    <a class="vs center l-flex-content link all-matches" href="/tournament/match?id=ID_DU_MATCH">
+                        VS
+                    </a>
+                    <p class="vs-date center l-flex-content all-matches">
+                        11.10 am - Tuesday 02/18/22
+                    </p>
+                </div>
+                <div class="team2 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+            </li>
+            <li class="match l-flex l-list3">
+                <div class="team1 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+                <div class="team-vs l-flex-item l-flex3-item center-vert">
+                    <a class="vs center l-flex-content link all-matches" href="/tournament/match?id=ID_DU_MATCH">
+                        VS
+                    </a>
+                    <p class="vs-date center l-flex-content all-matches">
+                        11.10 am - Tuesday 02/18/22
+                    </p>
+                </div>
+                <div class="team2 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+            </li>
+            <li class="match l-flex l-list3">
+                <div class="team1 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+                <div class="team-vs l-flex-item l-flex3-item center-vert">
+                    <a class="vs center l-flex-content link all-matches" href="/tournament/match?id=ID_DU_MATCH">
+                        VS
+                    </a>
+                    <p class="vs-date center l-flex-content all-matches">
+                        11.10 am - Tuesday 02/18/22
+                    </p>
+                </div>
+                <div class="team2 l-flex-item l-flex3-item">
+                    <img class="team-logo logo image all-matches" src="https://via.placeholder.com/256x256" alt="TeamName Logo" width="256px">
+                    <a class="title title-first_box team-name link all-matches" href="/wiki/TeamName">
+                        TeamName
+                    </a>
+                </div>
+            </li>
+        </ul>
+        <a class="btn btn-primary center link" href="/tournament/all-matches">See all matches</a>
+    </div>
+</div>
+<div class="team l-part">
+    <h2 class="title title-secondary">
+        Teams
+    </h2>
+    <p class="subtitle">
+        Every Teams!
+    </p>
+    <div class="l-flex l-list5">
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+        <div class="l-flex-item l-flex5-item">
+            <img class="team-logo logo image" src="https://via.placeholder.com/128x128" alt="TeamName Logo" width="128px">
+            <a class="team-name link" href="/wiki/TeamName">
+                TeamName
+            </a>
+        </div>
+    </div>
+</div>
+</div>
