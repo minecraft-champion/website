da053ebc7b2decb37fe3563080b85f4c60ff1a58
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-01-26 19:41:35 +0100

    feat env var for discord's link

--- public/index.php
+++ public/index.php
@@ -5,18 +5,20 @@ use App\Rooter\Rooter;
 use App\Rooter\HeaderCreator;
 
 $name = "E-Sport PvP Minecraft";
+$discord = "pas de lien";
 
 $header = new HeaderCreator($_SERVER['REQUEST_URI'], "Imaginez une véritable compétition PvP sur Minecraft", "/img/background/arena.jpg");
 $header->mapTitleArray([
     "/404" => "Error 404",
     "/about" => "Qui sommes-nous ?",
-    "/discord" => "Rejoignez nous sur Discord !"
+    "/discord" => "Rejoignez nous sur <a href='$discord' class='link link-extern' target='_blank'>Discord</a> !"
 ]);
 
 $rooter = new Rooter($_SERVER['REQUEST_URI'], $header, 'elements');
 $rooter->setSiteName($name);
 $rooter->setEnvVar([
-    "name" => $name
+    "name" => $name,
+    "discord" => $discord
 ]);
 
 $rooter->root();
