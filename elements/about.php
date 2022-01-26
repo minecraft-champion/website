93aad57fe0b6d980165b01dc403b45830d2a6349
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-01-26 18:51:31 +0100

    feat about's page

---
+++ elements/about.php
@@ -0,0 +1,84 @@
+<div class="about">
+<div class="presentation l-part l-no-marge background-color2">
+    <h2 class="title title-secondary">
+        Nous sommes justes :
+    </h2>
+    <ul class="list little-list">
+        <li class="list-item">Des Fans de Minecaft</li>
+        <li class="list-item">Des Gros Joueurs PvP</li>
+        <li class="list-item">Des Développeurs</li>
+        <li class="list-item">Des Builders</li>
+        <li class="list-item">Des Sysadmins</li>
+        <li class="list-item">Des Animateurs</li>
+        <li class="list-item">Bref, des gens normaux</li>
+    </ul>
+</div>
+<div class="staff l-part">
+    <h2 class="title title-secondary">
+        Notre Staff
+    </h2>
+    <div class="l-flex l-list3">
+        <div class="l-flex3-item l-flex-item">
+            <img src="/img/avatar/xav.png" alt="Avatar de Xav" class="image avatar">
+            <h3 class="title title-first_box">
+                Xav
+            </h3>
+            <p class="subtitle">
+                Fondateur de CMFr
+            </p>
+            <p class="description center">
+                Je le laisse faire une description de lui ici
+            </p>
+            <p class="description center">
+                Il n'aime pas le PvP mais il avait une commue de 7k et un ami à aider
+            </p>
+        </div>
+        <div class="l-flex3-item l-flex-item">
+            <img src="/img/avatar/anhgelus.png" alt="Avatar d'Anhgelus" class="image avatar">
+            <h3 class="title title-first_box">
+                Anhgelus Morhtuuzh
+            </h3>
+            <p class="subtitle">
+                Fondateur et directeur de <?= $envVar["name"] ?>
+            </p>
+            <p class="description center">
+                Minecraftien depuis 2011, adicte depuis 2012, pciste depuis 2015 et joueur pvp depuis 2019/2020.
+            </p>
+            <p class="description center">
+                Grand Master sur <a href="https://rinaorc.com/" class="link link-extern" target="_blank">Rinaorc</a>, KDR à 1.2 et FKDR à 2.0 sur <a href="https://hypixel.net/bedwars" class="link link-extern" target="_blank">Hypixel</a> Bedwars.
+            </p>
+        </div>
+        <div class="l-flex3-item l-flex-item">
+            <img src="/img/avatar/max.png" alt="Avatar de Max" class="image avatar">
+            <h3 class="title title-first_box">
+                Max
+            </h3>
+            <p class="subtitle">
+                Administrateur sur <?= $envVar["name"] ?>
+            </p>
+            <p class="description center">
+                Je le laisse faire un desc de lui ici
+            </p>
+            <p class="description center italic">
+                Il n'aime pas le PvP mais il aime organiser et développer
+            </p>
+        </div>
+        <div class="l-flex3-item l-flex-item">
+            <img src="/img/avatar/rastarocket.png" alt="Avatar de RastaRocket" class="image avatar">
+            <h3 class="title title-first_box">
+                RastaRocket
+            </h3>
+            <p class="subtitle">
+                Lead Builder sur <?= $envVar["name"] ?>
+            </p>
+            <p class="description center">
+                Je le laisse faire un desc de lui ici
+            </p>
+            <p class="description center italic">
+                Il aime le PvP et le Badlion (mais Lunar>Badlion)
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+</div>
