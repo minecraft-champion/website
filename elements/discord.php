6c2ad4edd8d933bee56806a1f9befaf3e4c9865c
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-01-26 19:39:51 +0100

    feat discord's page

---
+++ elements/discord.php
@@ -0,0 +1,24 @@
+<div class="discord">
+<div class="presentation l-part l-no-marge background-color2">
+    <h2 class="title title-secondary">
+        Discord ?
+    </h2>
+    <p class="subtitle">
+        <a href="https://discord.com/" class="link link-extern" target="_blank">Discord</a> est une messagerie instantanée gratuite designé pour les G@M3RZ !
+    </p>
+</div>
+<div class="why-join l-part">
+    <h2 class="title title-secondary">
+        Pourquoi rejoindre Discord ?
+    </h2>
+    <p class="subtitle">
+        Sur Discord, vous allez pouvoir suivre les news en temps réel, pouvoir créer votre équipe, voir les dernières infos des équipes, et bien plus encore !
+    </p>
+    <a class="btn-primary btn link link-extern" href="https://discord.com/" target="_blank">
+        Installer Discord
+    </a>
+    <a class="btn btn-primary link link-extern" href="<?= $envVar["discord"] ?>" target="_blank">
+        Le Discord de <?= $envVar["name"] ?>
+    </a>
+</div>
+</div>
